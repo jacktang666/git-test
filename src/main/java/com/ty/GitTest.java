91a862e3556b512d1dc41453a64baf6c5aa09369
ty <simth57586@gmail.com>
2022-04-06 22:41:02 +0800

    first commit

---
+++ src/main/java/com/ty/GitTest.java
@@ -0,0 +1,14 @@
+package com.ty;
+
+/**
+ * @program: git-test
+ * @description: git测试
+ * @author: tang yu
+ * @create: 2022-04-06 22:37
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
